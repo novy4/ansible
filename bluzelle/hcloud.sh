8db1e4e7ad2df83ffc6fcda3dcd908740874da7a
Alex Novacovschi <alex.novacovschi@endava.com>
2020-05-23 17:14:25 +0300

    bluzelle added

---
+++ bluzelle/hcloud.sh
@@ -0,0 +1,15 @@
+### DC1 ###
+hcloud server create --datacenter fsn1-dc14 --name sentry1.bluzelle.novy.pw --ssh-key 1569977  --type cpx21 --image 168855
+hcloud server create --datacenter fsn1-dc14 --name ded.bluzelle.novy.pw --ssh-key 1569977  --type cx21 --image 168855
+hcloud server create --datacenter fsn1-dc14 --name dude.bluzelle.novy.pw --ssh-key 1569977  --type cx21 --image 168855
+hcloud server create --datacenter fsn1-dc14 --name vasily.bluzelle.novy.pw --ssh-key 1569977  --type cx21 --image 168855
+hcloud server create --datacenter fsn1-dc14 --name sergey.bluzelle.novy.pw --ssh-key 1569977  --type cx21 --image 168855
+
+### DC2 ###
+hcloud server create --datacenter hel1-dc2 --name sentry2.bluzelle.novy.pw --ssh-key 1569977  --type cpx21 --image 168855
+hcloud server create --datacenter hel1-dc2 --name mihail.bluzelle.novy.pw --ssh-key 1569977  --type cx21 --image 168855
+hcloud server create --datacenter hel1-dc2 --name alexander.bluzelle.novy.pw --ssh-key 1569977  --type cx21 --image 168855
+hcloud server create --datacenter hel1-dc2 --name roman.bluzelle.novy.pw --ssh-key 1569977  --type cx21 --image 168855
+hcloud server create --datacenter hel1-dc2 --name ivan.bluzelle.novy.pw --ssh-key 1569977  --type cx21 --image 168855
+
+
